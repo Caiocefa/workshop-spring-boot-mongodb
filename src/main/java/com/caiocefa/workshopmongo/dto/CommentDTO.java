cb2595b251a5f434666c325234ae56b74289492c
Caio Cesar <caiocesarfandrade@gmail.com>
2022-04-05 22:21:49 -0300

    Consulta simples com query methods

--- src/main/java/com/caiocefa/workshopmongo/dto/CommentDTO.java
+++ src/main/java/com/caiocefa/workshopmongo/dto/CommentDTO.java
@@ -1,8 +1,10 @@
 package com.caiocefa.workshopmongo.dto;
 
+import java.io.Serializable;
 import java.util.Date;
 
-public class CommentDTO {
+public class CommentDTO implements Serializable{
+	private static final long serialVersionUID = 1L;
 	
 	private String text;
 	private Date date;
--- src/main/java/com/caiocefa/workshopmongo/services/PostService.java
+++ src/main/java/com/caiocefa/workshopmongo/services/PostService.java
@@ -1,10 +1,10 @@
 package com.caiocefa.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import com.caiocefa.workshopmongo.domain.Post;
 import com.caiocefa.workshopmongo.repository.PostRepository;
@@ -16,9 +16,13 @@ public class PostService {
 	@Autowired
 	private PostRepository repo;
 	
-	public Post findById(@PathVariable String id) {
+	public Post findById(String id) {
 		Optional<Post> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public List<Post> findByTitle(String text){
+		return repo.findByTitleContainingIgnoreCase(text);
 	}
 
 }
